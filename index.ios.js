0e2eaf013daf39f30a0add4875655dcd9dec0634
dllo <dllo@dllodeMac-mini-113.local>
2017-02-08 16:04:38 +0800

    老板生气了

--- index.ios.js
+++ index.ios.js
@@ -11,7 +11,10 @@ import {
   Text,
   View
 } from 'react-native';
-var my = "网络上修改的数据"
+
+
+var my = "网络上修改的数据";
+var you = "请到财务去交罚款";
 var myNew = "嘿嘿";
 var Main = require("./component/Main");
 
